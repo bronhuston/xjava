16286dd8036a62da4aeab53a630f54bec6616a56
Order Portal <pivotal@highland.pivotal.io>
2016-04-01 08:35:43 -0700

    rvm use in non-interactive mode.

--- bin/journey-setup.sh
+++ bin/journey-setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 rvm install 2.2.1
-rvm use 2.2.1
+rvm 2.2.1 do rvm env --path
+source $(rvm 2.2.1 do rvm env --path)
+
 git clone https://github.com/exercism/x-api
 cd x-api
 git submodule init -- tracks/java
