0f20ee5e1aa3104711c8b501f35717096171f51c
Order Portal <pivotal@highland.pivotal.io>
2016-04-08 07:45:37 -0700

    Check what rackup processes are running in the background

--- bin/journey-setup.sh
+++ bin/journey-setup.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-rvm install 2.2.1
-rvm 2.2.1 do rvm env --path
-source $(rvm 2.2.1 do rvm env --path)
-
 git clone https://github.com/exercism/x-api
 cd x-api
 git submodule init -- tracks/java
@@ -12,5 +8,7 @@ git submodule update
 gem install bundler
 bundle install
 rackup --host 127.0.0.1&
+
+ps aux | grep rackup
 
 curl 'localhost:9292/tracks/java/hello-world'
