9d1171bed10d4c85b27b13616c9ae958424ef0c3
Dan Neumann <dan@bestangle.com>
2016-05-20 08:07:10 -0700

    Fetch an Exercise from local X-API
    - Create local and ci versions of build script
    
    Signed-off-by: Bronwyn Perry-Huston <bronhuston@gmail.com>

---
+++ bin/journey-setup-ci.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+git clone https://github.com/exercism/x-api
+cd x-api
+git submodule init -- metadata
+git submodule init -- tracks/java
+git submodule update
+
+gem install bundler
+bundle install
+
+RACK_ENV=development rackup&
+
+sleep 5
+
+export GOPATH=$HOME/build/bronhuston
+export PATH=$PATH:$GOPATH/bin
+go get -u github.com/exercism/cli/exercism
+exercism -v
+
+cd ~
+mkdir -p workspace/exercism/exercises
+cd ~/workspace/exercism/exercises
+exercism configure --dir=~/workspace/exercism/exercises
+exercism configure --api http://localhost:9292
+exercism debug
+exercism fetch java bob
+tree java
--- bin/journey-setup-local.sh
+++ bin/journey-setup-local.sh
@@ -13,7 +13,16 @@ RACK_ENV=development rackup&
 
 sleep 5
 
-export GOPATH=$HOME/build/bronhuston
+export GOPATH=$HOME/workspace
 export PATH=$PATH:$GOPATH/bin
 go get -u github.com/exercism/cli/exercism
 exercism -v
+
+cd ~
+mkdir -p workspace/exercism/exercises
+cd ~/workspace/exercism/exercises
+exercism configure --dir=~/workspace/exercism/exercises
+exercism configure --api http://localhost:9292
+exercism debug
+exercism fetch java bob
+tree java
