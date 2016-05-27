5dceaceb2809b9877bcf59a9797d7345dbd1c668
Bronwyn Perry-Huston <bronhuston@gmail.com>
2016-05-27 07:16:40 -0700

    Try tcpdump to debug the network issues.
    Signed-off-by: Cen Ge <cge@corelogic.com>

--- bin/journey-setup-ci.sh
+++ bin/journey-setup-ci.sh
@@ -27,7 +27,8 @@ exercism configure --dir=~/workspace/exercism/exercises
 exercism configure --api http://localhost:9292
 
 curl -v 'localhost:9292/v2/exercises/java/bob'
-
+tcpdump -vv -c 20 -s 0 -i lo0 -A tcp port 9292 > out.txt &
 exercism --verbose debug
 exercism --verbose fetch java bob
 tree java
+cat out.txt
