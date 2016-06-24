675224b9d94ef93b33fda53928cb2c28116cfcd7
Cen Ge <cge@corelogic.com>
2016-06-24 06:49:28 -0700

    Copy solution to Bob and run the tests
    Signed-off-by: Bronwyn Perry-Huston <bronhuston@gmail.com>

--- bin/journey-setup-ci.sh
+++ bin/journey-setup-ci.sh
@@ -2,6 +2,8 @@
 
 eval "$(curl -sL https://raw.githubusercontent.com/travis-ci/gimme/master/gimme | GIMME_GO_VERSION=1.5.3 bash)"
 
+mkdir -p ~/workspace/exercism
+cd ~/workspace/exercism/
 git clone https://github.com/exercism/x-api
 cd x-api
 git submodule init -- metadata
@@ -21,9 +23,7 @@ export PATH=$PATH:$GOPATH/bin
 go get -u github.com/exercism/cli/exercism
 exercism -v
 
-cd ~
-pwd
-mkdir -p workspace/exercism/exercises
+mkdir -p ~/workspace/exercism/exercises
 cd ~/workspace/exercism/exercises
 exercism configure --dir=~/workspace/exercism/exercises
 exercism configure --api http://localhost:9292
@@ -31,3 +31,7 @@ exercism configure --api http://localhost:9292
 exercism debug
 exercism fetch java bob
 tree java
+
+cp ~/workspace/exercism/xjava/exercises/bob/src/example/java/* ~/workspace/exercism/exercises/java/bob/src/main/java/
+cd ~/workspace/exercism/exercises/java/bob/
+gradle test
--- bin/journey-setup-local.sh
+++ bin/journey-setup-local.sh
@@ -2,12 +2,15 @@
 
 go version
 
+mkdir -p ~/workspace/exercism
+cd ~/workspace/exercism/
 git clone https://github.com/exercism/x-api
 cd x-api
 git submodule init -- metadata
 git submodule init -- tracks/java
 git submodule update
 
+rbenv local 2.2.1
 gem install bundler
 bundle install
 
@@ -20,8 +23,7 @@ export PATH=$PATH:$GOPATH/bin
 go get -u github.com/exercism/cli/exercism
 exercism -v
 
-cd ~
-mkdir -p workspace/exercism/exercises
+mkdir -p ~/workspace/exercism/exercises
 cd ~/workspace/exercism/exercises
 exercism configure --dir=~/workspace/exercism/exercises
 exercism configure --api http://localhost:9292
@@ -29,3 +31,7 @@ exercism configure --api http://localhost:9292
 exercism debug
 exercism fetch java bob
 tree java
+
+cp ~/workspace/exercism/xjava/exercises/bob/src/example/java/* ~/workspace/exercism/exercises/java/bob/src/main/java/
+cd ~/workspace/exercism/exercises/java/bob/
+gradle test
